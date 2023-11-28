5002f575f3121d0e1c8b437e25856df148ba231d
Natanael Alves Gabriel <natasnael2002@gmail.com>
2023-11-27 21:58:37 -0300

    added new var to .env

--- src/connection/index.js
+++ src/connection/index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 async function main() {
-  await mongoose.connect(`mongodb+srv://TheNatas:${process.env.MONGOOSE_KEY}@cluster0.kignbiq.mongodb.net/?retryWrites=true&w=majority`)
+  await mongoose.connect(`mongodb+srv://${process.env.MONGOOSE_USER}:${process.env.MONGOOSE_KEY}@cluster0.kignbiq.mongodb.net/?retryWrites=true&w=majority`)
   console.log('Conectou com Mongoose!')
 }
 
