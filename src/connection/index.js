7076a1f2c5245675d4e4f682ef0406357a7ed273
Natanael Alves Gabriel <natasnael2002@gmail.com>
2023-11-27 21:54:43 -0300

    add .env file for password

--- src/connection/index.js
+++ src/connection/index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 async function main() {
-  await mongoose.connect('mongodb+srv://TheNatas:abacaterinoceronte@cluster0.kignbiq.mongodb.net/?retryWrites=true&w=majority')
+  await mongoose.connect(`mongodb+srv://TheNatas:${process.env.MONGOOSE_KEY}@cluster0.kignbiq.mongodb.net/?retryWrites=true&w=majority`)
   console.log('Conectou com Mongoose!')
-  
 }
 
 main().catch((err) => console.log(err))
